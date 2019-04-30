bf3286259c102b1d238e8adf16fe94c0b4003642
jondorito <jonsk8r10@gmail.com>
2019-04-29 21:25:24 -0600

    Networking and libraries directories have been added
    Class NetPackage has been added
    rapidjson has been added to the project

---
+++ Server/Networking/NetPackage.h
@@ -0,0 +1,30 @@
+//
+// Created by jondorito on 29/04/19.
+//
+
+#ifndef GLADIATORS_GBP_NETPACKAGE_H
+#define GLADIATORS_GBP_NETPACKAGE_H
+
+
+#include <string>
+
+class NetPackage {
+private:
+    std::string from = "unassigned";
+    std::string command = "NONE";
+    std::string data =  "NONE";
+public:
+    NetPackage();
+    std::string getJSONPackage();
+    std::string getFrom();
+    std::string getData();
+    std::string getcommand();
+    void setFrom(std::string);
+    void setData(std::string);
+    void setCommand(std::string);
+
+};
+
+
+
+#endif //GLADIATORS_GBP_NETPACKAGE_H
