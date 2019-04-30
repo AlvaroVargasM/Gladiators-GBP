100bc3bd28ca3ea19eaae5fac6cf2e095064a525
jondorito <jonsk8r10@gmail.com>
2019-04-29 21:33:03 -0600

    Doxygen documentation for Netpackage added
    Server's still missing both documentation and tweaks to configure it
    for the task

--- Server/Networking/NetPackage.cpp
+++ Server/Networking/NetPackage.cpp
@@ -14,6 +14,11 @@
 NetPackage::NetPackage() {
 }
 
+
+/**
+ * This function is in charge of constructing the json string
+ * @return Json String of this NetPackage object
+ */
 std::string NetPackage::getJSONPackage(){
     rapidjson::Document docu;
 
@@ -34,26 +39,53 @@ std::string NetPackage::getJSONPackage(){
     return strbuf.GetString();
 }
 
+
+/**
+ * Gets the author of the package
+ * @return The author of this package
+ */
 std::string NetPackage::getFrom() {
     return this->from;
 }
 
+
+/**
+ * Gets the data of the Package
+ * @return Data of the package
+ */
 std::string NetPackage::getData() {
     return this->data;
 }
 
+
+/**
+ * Gets the command associated with the package
+ * @return Command of this package
+ */
 std::string NetPackage::getcommand() {
     return this->command;
 }
 
+/**
+ * Sets the author of the package
+ * @param author
+ */
 void NetPackage::setFrom(std::string form) {
     this->from = form;
 }
 
+/**
+ * Sets the data for the package
+ * @param data ( as a string )
+ */
 void NetPackage::setData(std::string data) {
     this->data = data;
 }
 
+/**
+ * Sets the command for the package
+ * @param command
+ */
 void NetPackage::setCommand(std::string command) {
     this->command = command;
 }
