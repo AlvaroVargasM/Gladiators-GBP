42976b17fe90bdd969d4c1c3aab16291c990c9d1
AlvaroVargasM <valvaro707@gmail.com>
2019-05-09 17:35:17 -0600

    Added Doxygen to the RNG class

--- Server/Logic/RNG.h
+++ Server/Logic/RNG.h
@@ -2,18 +2,27 @@
 // Created by alvar on 05/05/19.
 //
 
-#ifndef GA_RNG_H
-#define GA_RNG_H
+#pragma once
 
 #include <cstdlib>
 #include <time.h>
 #include <iostream>
 
+/**
+ * Random number generator class, it is used for the generation of random integers
+ */
 class RNG {
 public:
+    /**
+     * RNG default constructor
+     */
     RNG() = default;
-    int getRandomNumber(int first_Number,int max_Range);
+
+    /**
+     * Method for creating a random integer from a given range
+     * @param minimun_Number minimum number to generate
+     * @param max_Range maximum number to generate
+     * @return an int created from 0 to the max_range + the minimum number
+     */
+    int getRandomNumber(int minimun_Number, int max_Range);
 };
-
-
-#endif //GA_RNG_H
