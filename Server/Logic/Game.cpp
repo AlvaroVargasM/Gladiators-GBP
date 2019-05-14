8cdf1183d03b376dc946d61027bf03e7ecb1f372
AlvaroVargasM <valvaro707@gmail.com>
2019-05-14 15:01:51 -0600

    .

--- Server/Logic/Game.cpp
+++ Server/Logic/Game.cpp
@@ -59,36 +59,36 @@ void Game::putTower(int i, int j, int type) {
     }
     this->game_Zone->getZone(i,j)->setBlocked(true);
 
-    GenericLinkedList<int>* ahhhhh = new GenericLinkedList<int>;
-    ahhhhh->add(n_Towers);
-    ahhhhh->add(damage);
+    GenericLinkedList<int>* arrow = new GenericLinkedList<int>;
+    arrow->add(n_Towers);
+    arrow->add(damage);
 
-    this->game_Zone->getZone(i,j)->getDamage()->add(ahhhhh);
+    this->game_Zone->getZone(i,j)->getDamage()->add(arrow);
 
     for(int x = 1;x < (type + 1);x++) {
         if (i - x >= 0 && !this->game_Zone->getZone(i - x,j)->isBlocked())
-            this->game_Zone->getZone(i - x,j)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i - x,j)->getDamage()->add(arrow);
 
         if (i + x < N_ROWS && !this->game_Zone->getZone(i + x,j)->isBlocked())
-            this->game_Zone->getZone(i + x,j)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i + x,j)->getDamage()->add(arrow);
 
         if (j - x >= 0 && !this->game_Zone->getZone(i,j - x)->isBlocked())
-            this->game_Zone->getZone(i,j - x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i,j - x)->getDamage()->add(arrow);
 
         if (j + x < N_COLUMNS && !this->game_Zone->getZone(i,j + x)->isBlocked())
-            this->game_Zone->getZone(i,j + x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i,j + x)->getDamage()->add(arrow);
 
         if (i - x >= 0 && j - x >= 0 && !this->game_Zone->getZone(i - x,j - x)->isBlocked())
-            this->game_Zone->getZone(i - x,j - x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i - x,j - x)->getDamage()->add(arrow);
 
         if (i - x >= 0 && j + x < N_COLUMNS && !this->game_Zone->getZone(i - x,j + x)->isBlocked())
-            this->game_Zone->getZone(i - x,j + x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i - x,j + x)->getDamage()->add(arrow);
 
         if (i + x < N_ROWS && j - x >= 0 && !this->game_Zone->getZone(i + x,j - x)->isBlocked())
-            this->game_Zone->getZone(i + x,j - x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i + x,j - x)->getDamage()->add(arrow);
 
         if (i + x < N_ROWS && j + x < N_COLUMNS && !this->game_Zone->getZone(i + x,j + x)->isBlocked())
-            this->game_Zone->getZone(i + x,j + x)->getDamage()->add(ahhhhh);
+            this->game_Zone->getZone(i + x,j + x)->getDamage()->add(arrow);
     }
 }
 
@@ -144,6 +144,7 @@ void Game::generateNewGUITower(int i,int j,int type) {
 GenericLinkedList<std::string> Game::getTowers() {
     generateTowers();
     this->path_A = *this->pths.findPathByA_Star(this->game_Zone,INI_I,INI_J,FIN_I,FIN_J);
+    this->path_B = *this->pths.BackTrack(this->game_Zone,INI_I,INI_J,FIN_I,FIN_J);
     return new_GUI_Towers;
 }
 
@@ -199,16 +200,17 @@ GA &Game::getPoolB() {
 
 std::string Game::calculateSteps() {
     // RECORRO LA LISTA PARA CREAR UNA NUEVA DE HASTA DONDE LLEGA EL CHAMP
-
+    GenericLinkedList<Zone*> travel_A = resizePath(1);
+    GenericLinkedList<Zone*> travel_B = resizePath(2);
 
     // TRADUZCO A NUEVA LISTA A UN STRING
-
+    std::string result = translateTravel(travel_A,travel_B);
 
     this->pool_A.newGen(); // GENERATION
     this->pool_B.newGen(); // CHANGE
     this->saveGenStats();  // Save the data of the new gen
     this->new_GUI_Towers = GenericLinkedList<std::string>(); // Clears the list for the GUI towers
-    return "!202GAME";
+    return result;
 }
 
 GenericLinkedList<Zone*> Game::resizePath(int type) {
@@ -226,14 +228,92 @@ GenericLinkedList<Zone*> Game::resizePath(int type) {
                 cutPath.add(this->path_A.get(i)->getData());
             }
         }
+        if (cutPath.getLast()->getData()->getId() == this->path_A.getLast()->getData()->getId())
+            this->completed_A = true;
     }
 
-    if (type == 2) {//RESIZE Bt
+    if (type == 2) { // RESIZE Bt
+        int glife = this->pool_B.getStrongest().getResistance();
+
+        for (int i = 0;i < *this->path_B.getLength();i++) {
+            for (int x = 0; x < *this->path_B.get(i)->getData()->getDamage()->getLength();i++) {
+                glife -= this->path_B.get(i)->getData()->getDamage()->get(x)->getData()->get(1)->getData();
+                x++;
+            }
+            if (glife > 0) {
+                cutPath.add(this->path_B.get(i)->getData());
+            }
         }
+        if (cutPath.getLast()->getData()->getId() == this->path_B.getLast()->getData()->getId())
+            this->completed_B = true;
+    }
     else
         std::cout << "!219GAME";
 
     return cutPath;
+}
+
+std::string Game::translateTravel(GenericLinkedList<Zone *> travel_A,GenericLinkedList<Zone *> travel_B) {
+    std::string final_Command_Line; // Used for the completed string
+
+    int last_i = INI_I;
+    int last_j = INI_J;
+
+    GenericLinkedList<std::string> commands_A;
+    GenericLinkedList<std::string> commands_B;
+
+    // We fill the command list of A
+    for (int i = 1;i < *travel_A.getLength();i++) {
+        std::string command;
+        int current_i = travel_A.get(i)->getData()->getX(N_COLUMNS);
+        int current_j = travel_A.get(i)->getData()->getY(N_COLUMNS,N_ROWS);
+
+        if (*commands_A.getLength() == 0)
+            command += "move.a";
+        else
+            command += ".move.a";
+
+        if (current_i == last_i && current_j > last_j)
+            command += ".right";
+        if (current_i == last_i && current_j < last_j)
+            command += ".left";
+        if (current_i == last_i && current_j == last_j)
+            command += ".up";
+        if (current_i == last_i && current_j == last_j)
+            command += ".down";
+    }
+
+    // We fill the command list of B
+    for (int i = 1;i < *travel_B.getLength();i++) {
+        std::string command;
+        int current_i = travel_B.get(i)->getData()->getX(N_COLUMNS);
+        int current_j = travel_B.get(i)->getData()->getY(N_COLUMNS,N_ROWS);
+
+        command += ".move.b";
+
+        if (current_i == last_i && current_j > last_j)
+            command += ".right";
+        if (current_i == last_i && current_j < last_j)
+            command += ".left";
+        if (current_i == last_i && current_j == last_j)
+            command += ".up";
+        if (current_i == last_i && current_j == last_j)
+            command += ".down";
+    }
+
+    int index = 0;
+    if (*commands_A.getLength() == *commands_B.getLength())
+        index = *commands_A.getLength();
+    if (*commands_A.getLength() < *commands_B.getLength())
+        index = *commands_B.getLength();
+    if (*commands_A.getLength() > *commands_B.getLength())
+        index = *commands_A.getLength();
+
+    for (int i = 0;i < index;i++) {
+        final_Command_Line += commands_A.get(i)->getData();
+        final_Command_Line += commands_B.get(i)->getData();
+    }
+    return final_Command_Line;
 }
 
 bool Game::isCompletedA() {
--- Server/main.cpp
+++ Server/main.cpp
@@ -7,17 +7,8 @@
 #include "Networking/Server.h"
 
 int main () {
-    IntimidationZone* intimidationZone = new IntimidationZone(4, 4);
-    //intimidationZone->getZoneByID(5)->setBlocked(true);
-    //intimidationZone->getZoneByID(6)->setBlocked(true);
-    //intimidationZone->getZoneByID(7)->setBlocked(true);
-    //intimidationZone->getZoneByID(11)->setBlocked(true);
-
-    PathSolver* solver = new PathSolver;
-    GenericLinkedList<Zone*>* list = solver->BackTrack(intimidationZone, 0, 0, 3,2);
-    for(int i = 0; i < *list->getLength(); i++){
-        std::cout << list->get(i)->getData()->getId() << std::endl;
-    }
-    std::cout << solver->getA_starTime() << std::endl;
-  //  Server::start();
+    Game g;
+    g.getTowers();
+    std::cout << g.calculateSteps();
+    // Server::start();
 }
