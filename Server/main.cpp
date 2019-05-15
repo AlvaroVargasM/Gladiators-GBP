b066791b8033bed7b771b34c3f78908b1721d7ea
jonathanGarcia <jonagaru1@gmail.com>
2019-05-14 18:15:49 -0600

    update

--- Server/main.cpp
+++ Server/main.cpp
@@ -8,8 +8,41 @@
 
 int main () {
     Game g;
+    //g.getTowers();
+    //std::cout << g.calculateSteps();
+    // Server::start();
+    PathSolver *pathSolver;
+    IntimidationZone *intimidationZone = new IntimidationZone(10, 10);
 
-    g.getTowers();
+  //  intimidationZone->getZoneByID(2)->setBlocked(true);
+   intimidationZone->getZoneByID(1)->setBlocked(true);
+    intimidationZone->getZoneByID(4)->setBlocked(true);
+    intimidationZone->getZoneByID(9)->setBlocked(true);
+    intimidationZone->getZoneByID(8)->setBlocked(true);
+    intimidationZone->getZoneByID(11)->setBlocked(true);
 
-    std::cout << g.calculateSteps();
+
+
+
+    //intimidationZone->getZoneByID(5)->setBlocked(true);
+
+     GenericLinkedList<Zone*>*list= pathSolver->BackTrack(intimidationZone,0,0,9,9);
+    for(int i = 0; i < *list->getLength(); i++){
+
+        std:: cout<<list->get(i)->getData()->getId()<<"\n";
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
